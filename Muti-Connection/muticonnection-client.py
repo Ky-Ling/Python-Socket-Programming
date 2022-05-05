233c756fed5d6d3bbf2114da7be71c3b7af35650
GC <2845740149@qq.com>
2022-05-05 22:30:13 +0800

    Fix bugs

--- Muti-Connection/muticonnection-client.py
+++ Muti-Connection/muticonnection-client.py
@@ -17,24 +17,23 @@ def start_connections(host, port, num_conns):
         print(f"Starting connection {connid} to {server_addr}")
 
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        
+
         # Each socket is set to non-blocking mode.
         sock.setblocking(False)
         sock.connect_ex(server_addr)
-        
+
         events = selectors.EVENT_READ | selectors.EVENT_WRITE
 
         # The data you want to store with the socket is created using SimpleNamespace.
-        # Everything needed to keep track of what the client needs to send, has sent, and has received, 
+        # Everything needed to keep track of what the client needs to send, has sent, and has received,
         #   including the total number of bytes in the messages, is stored in the object data.
         data = types.SimpleNamespace(
-            connid = connid,
-            msg_total = sum(len(m) for m in messages),
-            recv_total = 0,
-
+            connid=connid,
+            msg_total=sum(len(m) for m in messages),
+            recv_total=0,
             # The messages that the client will send to the server are copied
-            messages = messages.copy(),
-            outb = b""
+            messages=messages.copy(),
+            outb=b"",
         )
 
         sel.register(sock, events, data=data)
@@ -49,6 +48,8 @@ If the client doesn't close, the server will leave the connection open. In a rea
 you may want to guard against this in your server by implementing a timeout to prevent client connections from accumulating 
 if they don't send a request after a certain amount of time.
 """
+
+
 def service_connection(key, mask):
     sock = key.fileobj
     data = key.data
@@ -80,3 +81,23 @@ def service_connection(key, mask):
             data.outb = data.outb[sent:]
 
 
+if len(sys.argv) != 4:
+    print(f"Usage: {sys.argv[0]} <host> <port> <num_connections>")
+    sys.exit(1)
+
+host, port, num_conns = sys.argv[1:4]
+start_connections(host, int(port), int(num_conns))
+
+try:
+    while True:
+        events = sel.select(timeout=1)
+        if events:
+            for key, mask in events:
+                service_connection(key, mask)
+        # Check for a socket being monitored to continue.
+        if not sel.get_map():
+            break
+except KeyboardInterrupt:
+    print("Caught keyboard interrupt, exiting")
+finally:
+    sel.close()
--- Muti-Connection/muticonnection-server.py
+++ Muti-Connection/muticonnection-server.py
@@ -5,6 +5,56 @@ import types
 
 sel = selectors.DefaultSelector()
 
+
+def accept_wrapper(sock):
+
+    # Because the listening socket was registered for the event selectors.EVENT_READ, it should be ready to read.
+    conn, addr = sock.accept()
+    print(f"Accepted connection from {addr}")
+            
+    # Put the socket in non-blocking mode.
+    conn.setblocking(False)
+            
+    # Create an object to hold the data that we want included along with the socket using a SimpleNamespace. 
+    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(conn, events, data=data)
+
+        
+# How a client connection is handled when it¡¯s ready
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+
+    if mask & selectors.EVENT_READ:
+
+        # Should be ready to read
+        recv_data = sock.recv(1024)
+
+        if recv_data:
+            data.outb += recv_data
+                
+        # If no data is received, this means that the client has closed their socket, so the server should too.
+        else:
+            print(f"Closing connection to {data.addr}")
+            sel.unregister(sock)
+            sel.close()
+
+    if mask & selectors.EVENT_WRITE:
+        if data.outb:
+            print(f"Echoing {data.outb!r} to {data.addr}")
+                    
+            # Should be ready to write
+            sent = sock.send(data.outb)
+            data.outb = data.outb[sent:]
+
+
+
+if len(sys.argv) != 3:
+    print(f"Usage: {sys.argv[0]} <host> <port>")
+    sys.exit(1)
+
+
 host, port = sys.argv[1], int(sys.argv[2])
 lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 lsock.bind((host, port))
@@ -25,7 +75,7 @@ try:
         # Each tuple contains a key and a mask.
         events = sel.select(timeout=None)
 
-        #  key.fileobj is the socket object, and mask is an event mask of the operations that are ready.
+        # key.fileobj is the socket object, and mask is an event mask of the operations that are ready.
         for key, mask in events:
 
             # The data is from the listening socket and we need to accept the connection
@@ -34,54 +84,11 @@ try:
                 # Get the new socket object and register it with the selector
                 accept_wrapper(key.fileobj)
 
-            # It is a client socket that¡¯s already been accepted
+            # It is a client socket that is already been accepted
             else:
 
-                # Called with key and mask as arguments, and that¡¯s everything we need to operate on the socket.
+                # Called with key and mask as arguments, and that is everything we need to operate on the socket.
                 service_connection(key, mask)
-
-
-        def accept_wrapper(sock):
-
-            # Because the listening socket was registered for the event selectors.EVENT_READ, it should be ready to read.
-            conn, addr = sock.accept()
-            print(f"Accepted connection from {addr}")
-            
-            # Put the socket in non-blocking mode.
-            conn.setblocking(False)
-            
-            # Create an object to hold the data that we want included along with the socket using a SimpleNamespace. 
-            data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
-            events = selectors.EVENT_READ | selectors.EVENT_WRITE
-            sel.register(conn, events, data=data)
-
-        
-        # How a client connection is handled when it¡¯s ready
-        def service_connection(key, mask):
-            sock = key.fileobj
-            data = key.data
-
-            if mask & selectors.EVENT_READ:
-
-                # Should be ready to read
-                recv_data = sock.recv(1024)
-
-                if recv_data:
-                    data.outb += recv_data
-                
-                # If no data is received, this means that the client has closed their socket, so the server should too.
-                else:
-                    print(f"Closing connection to {data.addr}")
-                    sel.unregister(sock)
-                    sel.close()
-
-            if mask & selectors.EVENT_WRITE:
-                if data.outb:
-                    print(f"Echoing {data.outb!r} to {data.addr}")
-                    
-                    # Should be ready to write
-                    sent = sock.send(data.outb)
-                    data.outb = data.outb[sent:]
        
 except KeyboardInterrupt:
     print("Caught keyboard interrupt, exiting")
